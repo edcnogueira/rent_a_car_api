e9ab38da28ecad396a3daab5430bf2970fd6a4ad
Edjancarlos <edjan.nogueira14@gmail.com>
2022-10-10 00:09:08 -0300

    feat: implementa o list.

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,4 +1,4 @@
-import { Router } from "express";
+import { response, Router } from "express";
 
 import { CategoriesRespository } from "../repositories/CategoriesRepository";
 
@@ -13,6 +13,12 @@ categoriesRoutes.post("/", (req, res) => {
   categoriesRespository.create({ name, description });
 
   return res.status(201).send();
+});
+
+categoriesRoutes.get("/", (req, res) => {
+  const listCategories = categoriesRespository.list();
+
+  res.status(200).json({ listCategories });
 });
 
 export { categoriesRoutes };
