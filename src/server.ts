78cf2d3544cd64a29a0c6ad11c0af3294a9e8c71
Edjancarlos <edjan.nogueira14@gmail.com>
2022-10-19 08:26:00 -0300

    feat: configura swagger.

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,16 @@
 import express from "express";
+import sweggerUi from "swagger-ui-express";
+
 import { categoriesRoutes } from "./routes/categories.routes";
 import { specificationRoutes } from "./routes/specifications.routes";
+
+import swaggerFile from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", sweggerUi.serve, sweggerUi.setup(swaggerFile));
 
 app.use("/categories", categoriesRoutes);
 app.use("/specifications", specificationRoutes);
